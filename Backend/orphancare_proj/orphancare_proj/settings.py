de43f5b8bb2355ace6cc066e3936c64a6160a8fc
Akshay D S <akshayakki4898@gmail.com>
2025-11-13 00:37:58 +0530

    ne1w

--- Backend/orphancare_proj/orphancare_proj/settings.py
+++ Backend/orphancare_proj/orphancare_proj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-#ppx0@c1q6-esy^i-=ht8s350o6(xy^^kh)$i7tknroc)*ll3+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == "True"
 
 ALLOWED_HOSTS = ['orphancare-backend.onrender.com','127.0.0.1']
 
@@ -87,12 +88,24 @@ WSGI_APPLICATION = 'orphancare_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if not DEBUG:
+    DATABASES ={
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+
+       }
+    }
+
+
 
 
 # Password validation
@@ -129,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 
 # Default primary key field type
