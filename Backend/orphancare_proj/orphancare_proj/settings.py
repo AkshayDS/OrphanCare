85aad9bdbb5d5cbbf150e55312e773ed7eabde81
Akshay D S <akshayakki4898@gmail.com>
2025-11-13 01:06:11 +0530

    updated setting with static

--- Backend/orphancare_proj/orphancare_proj/settings.py
+++ Backend/orphancare_proj/orphancare_proj/settings.py
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
+# https://docs.djangoproject.com/en/5.2/topics/i18n/data
 
 LANGUAGE_CODE = 'en-us'
 
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 #STATIC_URL = 'static/'
 
 
