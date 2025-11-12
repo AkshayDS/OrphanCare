2cdbda2e05a54497c1c6f1bf63da833ec0c372fc
Akshay D S <119959400+AkshayDS@users.noreply.github.com>
2025-11-12 22:42:56 +0530

    updated allowed host

--- Backend/orphancare_proj/orphancare_proj/settings.py
+++ Backend/orphancare_proj/orphancare_proj/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-#ppx0@c1q6-esy^i-=ht8s350o6(xy^^kh)$i7tknroc)*ll3+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['orphancare-backend.onrender.com']
 
 
 # Application definition
